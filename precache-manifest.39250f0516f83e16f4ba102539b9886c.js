133e1c732828e6e2033b1a7633241b51bc67c85a
Stephen Axtell <axtell.stephen@gmail.com>
2019-10-03 15:49:51 +0200

    Updates

---
+++ precache-manifest.39250f0516f83e16f4ba102539b9886c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d914a05becd029a58198ead381f867",
+    "url": "/axtell5000/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "68117393b1d8500bc5cf",
+    "url": "/axtell5000/react-monster-rolodex/static/css/main.1f85a7d5.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/axtell5000/react-monster-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "68117393b1d8500bc5cf",
+    "url": "/axtell5000/react-monster-rolodex/static/js/main.3fceaf25.chunk.js"
+  },
+  {
+    "revision": "ef31ca2d674bd6cb907d",
+    "url": "/axtell5000/react-monster-rolodex/static/js/runtime-main.e8ac6bca.js"
+  }
+]);
